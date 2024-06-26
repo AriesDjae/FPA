7cd16d8032df24c33bfbb44edaca27b31c702fde
Muhammad Aries Djaenuri <ariesdjaenuri24@gmail.com>
2024-06-26 21:28:31 +0700

    Add files via upload

---
+++ SceneBuilder/demo/src/main/java/com/example/LoginController.java
@@ -0,0 +1,44 @@
+package com.example;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class LoginController {
+
+    @FXML
+    private Button Logbtn;
+
+    @FXML
+    private PasswordField passField;
+
+    @FXML
+    private CheckBox showpass;
+
+    @FXML
+    private TextField usernameField;
+
+    @FXML
+    void Loginnext(ActionEvent event) {
+
+    }
+
+    @FXML
+    void ShowPassword(ActionEvent event) {
+
+    }
+
+    @FXML
+    void fieldPassword(ActionEvent event) {
+
+    }
+
+    @FXML
+    void fieldUsername(ActionEvent event) {
+
+    }
+
+}
